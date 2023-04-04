bbdfa420d0c5d38febca1d9b9748487d0990a3a4
Jesus <deonatto@gmail.com>
2023-04-04 22:38:03 +0100

    redux setup and create some scenes files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Home from "scenes/home/Home";
+import Login from "scenes/login/Login";
+import Profile from "scenes/profile/Profile";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={Login} />
+          <Route path="/home" element={Home} />
+          <Route path="/profile/:id" element={Profile} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/state/store.js
@@ -0,0 +1,31 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import authReducer from "./auth";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export default store;
