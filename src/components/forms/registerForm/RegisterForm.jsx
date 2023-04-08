9d01571ffea1fffb7539acb27369308b8440cc52
Jesus <deonatto@gmail.com>
2023-04-08 20:02:03 +0100

    register form design

--- src/components/forms/registerForm/RegisterForm.jsx
+++ src/components/forms/registerForm/RegisterForm.jsx
@@ -1,18 +1,134 @@
 import React, { useState } from "react";
-import { Button, TextField } from "@mui/material";
+import {
+  Button,
+  TextField,
+  FormControl,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+  InputLabel,
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import "./RegisterForm.css";
+import { colorTokens } from "theme";
+import { ColorizeSharp } from "@mui/icons-material";
 
 const RegisterForm = () => {
-  const handleSubmit = () => {};
+  const [showPassword, setShowPassword] = useState(false);
+  const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [credentials, setCredentials] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    role: "admin",
+    password: "",
+  });
+  const changeHandler = (credentialName, e) => {
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [credentialName]: e.target.value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(credentials);
+  };
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="form-container">
-        <div className="register-container">
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <TextField label="First Name" />
-            <TextField label="Last Name" />
-            <TextField label="Role" />
-          </div>
+    <form className="register-form-container" onSubmit={handleSubmit}>
+      <h4 style={{ textDecoration: "none" }}>
+        Welcome, create your admin account and start creating polls.
+      </h4>
+      <div className="register-container">
+        <div
+          style={{
+            display: "flex",
+            gap: "0.5rem",
+            justifyContent: "space-between",
+          }}
+        >
+          <TextField
+            label="First Name"
+            onChange={(e) => changeHandler("firstName", e)}
+            sx={{
+              flex: 1,
+            }}
+          />
+          <TextField
+            label="Last Name"
+            onChange={(e) => changeHandler("lastName", e)}
+            sx={{
+              flex: 1,
+            }}
+          />
+        </div>
+        <TextField label="Email" onChange={(e) => changeHandler("email", e)} />
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={() => setShowPassword(!showPassword)}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            onChange={(e) => changeHandler("password", e)}
+            label="Password"
+          />
+        </FormControl>
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-repeat-password">
+            Repeat Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-repeat-password"
+            type={showRepeatPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={() => setShowRepeatPassword(!showRepeatPassword)}
+                  edge="end"
+                >
+                  {showRepeatPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            onChange={(e) => setRepeatPassword(e.target.value)}
+            error={
+              repeatPassword ? repeatPassword !== credentials.password : false
+            }
+            label="Repeat Password"
+          />
+        </FormControl>
+        <div>
+          <Button
+            fullWidth
+            type="submit"
+            sx={{
+              fontWeight: 'bold',
+              margin: "10px 0",
+              padding: "1rem",
+              backgroundColor: colorTokens.primary[500],
+              color: colorTokens.grey[0],
+              "&:hover": {
+                backgroundColor: colorTokens.primary[500],
+                color: colorTokens.grey[0],
+              },
+            }}
+          >
+            Register
+          </Button>
         </div>
       </div>
     </form>
--- src/scenes/login/Login.jsx
+++ src/scenes/login/Login.jsx
@@ -2,19 +2,27 @@ import React, { useState } from "react";
 import "./Login.css";
 import RegisterForm from "components/forms/registerForm/RegisterForm";
 import LoginForm from "components/forms/loginForm/LoginForm";
+import { colorTokens } from "theme";
 
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
 
   return (
     <div className="login-container">
-      <h1>Admin Votin App</h1>
-      {isLogin ? <LoginForm /> : <RegisterForm />}
-      <h4 onClick={() => setIsLogin(!isLogin)}>
-        {isLogin
-          ? "Don´t have an account? Click here."
-          : "Already have an account? Login here."}
-      </h4>
+      <div
+        className="login-title-container"
+        style={{ backgroundColor: colorTokens.primary[300] }}
+      >
+        <h1 style={{ color: colorTokens.grey[0] }}>Admin Votin App</h1>
+      </div>
+      <div className="form-container">
+        {isLogin ? <LoginForm /> : <RegisterForm />}
+        <h4 onClick={() => setIsLogin(!isLogin)}>
+          {isLogin
+            ? "Don´t have an account? Click here."
+            : "Already have an account? Login here."}
+        </h4>
+      </div>
     </div>
   );
 };
